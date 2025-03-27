0418abc1a71daf69d63149b49442c12edc7af250
tribo <tpoyrex2019@gmail.com>
2025-03-27 11:19:59 +0800

    restore pt

---
+++ DS/w5-2.cpp
@@ -0,0 +1,23 @@
+#include "ds_q5.hpp"
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> mapDamage(DamageNode_t *head, int m, int n) {
+	// Implement this function for assignment 5-1
+	return {};
+}
+
+vector<vector<int>> mapRotatingDamage(DamageNode_t *head, int m, int n, int k) {
+	// Implement this function for assignment 5-2
+	return {};
+}
+
+#ifndef OJ_MODE
+// This main function is for local testing only.
+// It will NOT be compiled or executed on the OJ system.
+int main() {
+    // TODO: Write test code here to verify your implementation.
+    return 0;
+}
+#endif
