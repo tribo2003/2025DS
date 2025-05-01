62da586fb01692c46026e2706c671bfc1b34107f
tribo2003 <145860184+tribo2003@users.noreply.github.com>
2025-05-01 07:53:02 +0000

    restore pt

--- DS/w7-2.cpp
+++ DS/w7-2.cpp
@@ -6,9 +6,28 @@ using namespace std;
 
 
 typedef long long ll;
+struct Node{
+    ll sum;
+    ll maxval;
+};
+
 vector<ll> zone;
-ll sum=0;
-ll maxval=0;
+vector<Node>segT;
+
+void build(int node, int l, int r){
+    if(l==r){
+        segT[node].sum=zone[l];
+        segT[node].maxval=zone[r];
+        return;
+    }
+    int mid=(l+r)/2;
+    build(2*node, l, mid);
+    build(2*node+1, mid+1, r);
+    segT[node].sum=segT[2*node].sum+segT[2*node+1].sum;
+    segT[node].maxval=max(segT[2*node].maxval, segT[2*node+1].maxval);
+}
+
+
 
 ll update(int index, ll value){
     ll ori=zone[index];
@@ -37,9 +56,11 @@ int main(){
     int n, x;
     cin>>n;
     zone.resize(n);
+    segT.resize(4*n);
     for(int i=0; i<n; i++){
         cin>>zone[i];
     }
+    build(1, 0, n-1);
 
     string op;
     cin>>x;
