a31403dc404ac3e013dfeea78190c20bdabbc926
tribo2003 <145860184+tribo2003@users.noreply.github.com>
2025-05-08 06:44:21 +0000

    restore pt

---
+++ DS/w8-1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+/*
+Q: 找 ＃of 朋友群
+單向linklist
+新匯入關係先找朋友在不在linklist裡：
+-在 加入現在的linklist
+-不在 加入新的linklist
+*使用tree儲存linklist:root指向各條linklist
+最後算root接了幾條link
+*/
+
+int main(){
+    int n, m;
+    cin>>n>>m;
+
+    for(int i=0; i<m; i++){
+        int rs, rf;
+        cin>>rs>>rf;
+    }
+
+
+    return 0;
+}
